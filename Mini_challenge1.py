7f1e538796f69b750cf7e28c35a57327bc033153
Cyph3rexe <67096012+Cyph3rexe@users.noreply.github.com>
2021-01-27 00:58:13 +0530

    A mini challenge which askes you to print some letters from a string using the Indexing feature.

---
+++ Mini_challenge1.py
@@ -0,0 +1,9 @@
+parrot = "Norwegian Blue"
+
+print(parrot)   # 'Prints Norwegian blue
+print(parrot[3])
+print(parrot[4])
+print()   # 'Leaves a blank space
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])    # 'Prints we win downwards
