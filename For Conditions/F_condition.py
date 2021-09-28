13fde3fd78a45ceed1296fb98102b5430c2f00a1
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-09-28 11:19:52 +0530

    Added some more files

---
+++ For Conditions/F_condition.py
@@ -0,0 +1,20 @@
+"""
+Creating the F symbol using for loops
+and a list. The output should look 
+something like this. 
+
+XXXXX
+XX
+XXXXX
+XX
+XX
+"""    
+
+#Declaring the list 
+nested = [5, 2, 5, 2, 2]
+
+#Intiating Loop
+for x in nested:
+    print(x*"x")
+
+#Sorry it's as easy as it seams
---
+++ Lists/names_lists.py
@@ -0,0 +1,11 @@
+"""
+Names of different people printed 
+using for loops. 
+"""
+
+#Declaring variables 
+names = ["Mosh", "Discord", 'Playstation', 'Sony']
+
+#Initiating For Loop 
+for i in names:
+    print(i)
