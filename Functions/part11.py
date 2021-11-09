9812a8ee6824204f7e814facd8d147887a9d2137
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-09 23:17:52 +0530

    question

---
+++ Functions/part11.py
@@ -0,0 +1,11 @@
+"""
+Write a program that contains user defined 
+functions to calculate area, perimeter or surface 
+area whichever is applicable for various shapes 
+like square, rectangle, triangle, circle and cylinder. 
+The user defined functions should accept the values 
+for calculation as parameters and the calculated 
+value should be returned. Import the module and 
+use the appropriate functions.
+"""
+
