46787da0e4904fd29dee2466e61c3efed3af6b6f
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-10 13:24:52 +0530

    initiated while loop

--- Functions/part11.py
+++ Functions/part11.py
@@ -34,14 +34,46 @@ def square():
         print("Please re-run the program.")
 
 def rectangle():
-    length = int(input("Enter the length of your rectangle: "))
-    breadth = int(input("Enter the breadth of your rectangle: "))
-    height = int(input("Enter the height of the rectangle: "))
-    rec_choice = int(input("""Here is your menu. Type the number
-    associated with the kind of value you want to see.
-    1 -> Perimeter
-    2 -> Area 
-    3 -> Volume 
-    4 -> Surface Area """))
+    while rec_choice < 5:
+        length = int(input("Enter the length of your rectangle: "))
+        breadth = int(input("Enter the breadth of your rectangle: "))
+        height = int(input("Enter the height of the rectangle: "))
+        rec_choice = int(input("""Here is your menu. Type the number
+        associated with the kind of value you want to see.
+        1 -> Perimeter
+        2 -> Area 
+        3 -> Volume 
+        4 -> Surface Area """))
 
+        if rec_choice == 1:
+            a1 = 2 * (length + breadth)
+            print(f"The perimeter of you rectangle is of {a1} cms.")
+        elif rec_choice == 2:
+            b1 = length * breadth
+            print(f"The area of your rectangle is {b1} cms^2")
+        elif rec_choice == 3:
+            c1 = length * breadth * height
+            print(f"The volume of your rectangle is {c1}")
+        elif rec_choice == 4:
+            d1 = 2 ((length * breadth) + (breadth * height) + (height * length))
+            print(f"The surface area of your rectangle is {d1}")
+        else:
+            print("Please try again")
 
+while main_choice < 6:
+    main_choice = int(input("""
+    Welcome.... 
+    This is a menu driven program to help you 
+    calculate stuff about shapes. 
+    Please choose the number associated with the 
+    values of the shape you want to see. 
+    1 -> Square
+    2 -> Rectangle
+    3 -> Triangle
+    4 -> Circle
+    5 -> Cylinder
+    Enter number here: """))
+    if main_choice == 1:
+        square()
+    elif main_choice == 2:
+        rectangle()
