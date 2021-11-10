bf080106a29301826addff5e1a33d656220d3991
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-10 12:55:09 +0530

    nice

--- Functions/part11.py
+++ Functions/part11.py
@@ -9,3 +9,39 @@ value should be returned. Import the module and
 use the appropriate functions.
 """
 
+def square():
+    side = int(input("Enter the value of the side of the square. (in cms): "))
+    sq_choice = int(input("""Here is your menu. Type the number
+    associated with the kind of value you want to see.
+    1 -> Perimeter
+    2 -> Area 
+    3 -> Volume 
+    4 -> Surface Area """))
+
+    if sq_choice == 1:
+        a = 4 * side
+        print(f"The perimeter is of value {a} cms")
+    elif sq_choice == 2:
+        b = side * side
+        print(f"The area is of value {b} cms^2")
+    elif sq_choice == 3:
+        c = side*side*side
+        print(f"The volume is of value {c} cms^3")
+    elif sq_choice == 4:
+        d = 6*side*side
+        print(f"The surface area is of value {d}")
+    else:
+        print("Please re-run the program.")
+
+def rectangle():
+    length = int(input("Enter the length of your rectangle: "))
+    breadth = int(input("Enter the breadth of your rectangle: "))
+    height = int(input("Enter the height of the rectangle: "))
+    rec_choice = int(input("""Here is your menu. Type the number
+    associated with the kind of value you want to see.
+    1 -> Perimeter
+    2 -> Area 
+    3 -> Volume 
+    4 -> Surface Area """))
+
+
