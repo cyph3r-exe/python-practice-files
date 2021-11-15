b5a1c15dc67831e58c80d313520e6a1dcba8eb25
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-15 09:15:39 +0530

    new condition file

---
+++ For Conditions/mltp_condition.py
@@ -0,0 +1,47 @@
+"""
+Make the following programs using user defined functions 
+( write down in your notebook and then try them on the system ) …
+ also pass arguments and return values in at least one question 
+(1) Fibonacci series 
+(2) return reverse of a number 
+(3) count the ni of spaces in a string
+"""
+
+choice_main = int(input("""
+What would you like to use in this program. 
+Please type the value associated with the 
+same. Thank you for co-operating. 
+1 -> Fibonacci till n Terms 
+2 -> Reverse of a number. 
+3 -> Number of spaces in a string. 
+
+Enter number here : 
+"""))
+
+def fibonacci():
+    # Program to display the Fibonacci sequence up to n-th term
+    nterms = int(input("How many terms? "))
+
+    # first two terms
+    n1, n2 = 0, 1
+    count = 0
+
+    # check if the number of terms is valid
+    if nterms <= 0:
+        print("Please enter a positive integer")
+    # if there is only one term, return n1
+    elif nterms == 1:
+        print("Fibonacci sequence upto",nterms,":")
+        print(n1)
+    # generate fibonacci sequence
+    else:
+        print("Fibonacci sequence:")
+    while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+
+
