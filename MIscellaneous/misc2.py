c5187912cd728e00cb06314811f2de7477e1c097
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-23 13:05:02 +0530

    new ques

---
+++ MIscellaneous/misc2.py
@@ -0,0 +1,8 @@
+"""
+ WAP to accept item name, price and category. 
+ Assign discount ( in percentage) according o the category given in the table.  
+ Thereafter display the total amount after subtracting the discount 
+ percentage from price of the item. 
+ The program should continue as long as user wishes to. 
+"""
+
