bf24345f17cf79b88ff891bdf0b475b140135dda
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-24 11:10:16 +0530

    new question

---
+++ MIscellaneous/misc5.py
@@ -0,0 +1,8 @@
+"""Write a program that has a user defined function 
+to accept the coefficients of a quadratic equation 
+in variables and calculates its determinant. For 
+example : if the coefficients are stored in the 
+variables a,b,c then calculate determinant as 
+b2-4ac. Write the appropriate condition to check 
+determinants on positive, zero and negative and 
+output appropriate result."""
