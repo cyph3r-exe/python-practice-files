58b59199b18e5e824978cf1d5522f38560c6446f
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-24 09:47:57 +0530

    added while loop

--- MIscellaneous/misc2.py
+++ MIscellaneous/misc2.py
@@ -6,8 +6,28 @@
  The program should continue as long as user wishes to. 
 """
 
-item = input("Enter the name of item you want to buy: ")
-price = int(input("Enter the price: "))
-category = int(input("""Enter the number of the category 
-you wish to put this in. 
-1 -> """))
+while x == 1:
+    item = input("Enter the name of item you want to buy: ")
+    price = int(input("Enter the price: "))
+    category = int(input("""Enter the number of the category 
+    you wish to put this in. 
+    1 -> Electronics
+    2 -> Kitchen 
+    3 -> Furniture"""))
+
+    if category == 1: 
+        discprice = price - ((5/100) * price)
+        print(f"The disocounted price for your {item} is {discprice}")
+    elif category == 2:
+        discprice = price - ((10/100) * price)
+        print(f"The disocounted price for your {item} is {discprice}")
+    elif category == 3:
+        discprice = price - ((5/100) * price)
+        print(f"The disocounted price for your {item} is {discprice}")
+    else:
+        print("Thank you for using our service")
+        x = input("Enter 1 if you would like to continue, or enter any other button to exit: ")
+        if x == 1:
+            continue
+        else:
+            break   
