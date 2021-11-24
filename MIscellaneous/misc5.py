e83f582301a3e91ae24d7ba1b44f4fd4a012a316
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-24 20:53:11 +0530

    written solution

--- MIscellaneous/misc5.py
+++ MIscellaneous/misc5.py
@@ -6,3 +6,28 @@ variables a,b,c then calculate determinant as
 b2-4ac. Write the appropriate condition to check 
 determinants on positive, zero and negative and 
 output appropriate result."""
+
+
+import os
+def test():
+    a = int(input("Enter the first coeficient: "))
+    b = int(input("Enter the second coefficient: "))
+    c = int(input("Enter the third coefficient: "))
+
+    calc = pow(b,2) - 4*(a*c)
+    if calc > 0:
+        print("The solution is positive")
+    elif calc == 0:
+        print("The solution is neutral")
+    elif calc < 0:
+        print("The solution is negative.")
+    else:
+        print("Plase try again.")
+
+x = input("Do you want to start the program? Enter Y for Yes and N for No.")
+if x == 'y' or x == 'Y':
+    test()
+elif x == 'N' or x == 'n':
+    os.exit
+else:
+    os.exit
