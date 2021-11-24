dc08660d151cd4c2a27d05a6576b13e6849b8104
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-24 11:05:43 +0530

    completed code

--- MIscellaneous/misc4.py
+++ MIscellaneous/misc4.py
@@ -1,4 +1,17 @@
 """
 WAP to accept a number and display sum of squares of its digits. 
 """
+print 
+try:
+    num = int(input("Enter a number: "))
+    temp = num
+    mix = 0
+    while temp != 0:
+        hi = temp % 10
+        low = hi * hi
+        mix = mix + low
+        temp = int(temp /10 )
+    print(f"\n Sum of square of digits of num{num} = {mix}")
 
+except ValueError:
+    print("\n Wrong input") 
