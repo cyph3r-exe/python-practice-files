3d3015b79298c4ed25bbfaf3e60af3dc3dbe045b
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-25 08:25:22 +0530

    question added

---
+++ MIscellaneous/misc6.py
@@ -0,0 +1,17 @@
+"""
+Write a program that creates a GK quiz consisting 
+of any five questions of your choice. The questions 
+should be displayed randomly. Create a user defined 
+function score() to calculate the score of the quiz and 
+another user defined function remark (scorevalue) 
+that accepts the final score to display remarks as 
+follows:
+
+5 Outstanding
+4 Excellent
+3 Good
+2 Read more to score more
+1 Needs to take interest
+0 General knowledge will always help you. Take it seriously.
+"""
+
