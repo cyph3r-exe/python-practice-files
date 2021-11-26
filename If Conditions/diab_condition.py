264de8e071fbd167db69ac1b9d310c8770002367
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-26 11:39:28 +0530

    inputs

--- If Conditions/diab_condition.py
+++ If Conditions/diab_condition.py
@@ -3,3 +3,13 @@ Python program to determine whether someone
 has diabetes or not. 
 """
 
+#taking all inputs
+glucose_conc = int(input("Enter your glucose concentration: "))
+bMi = int(input("Enter your BMI: "))
+Age = int(input("Enter your Age: "))
+blood_pressure = int(input("Enter blood pressure: "))
+skin_thik = int(input("Enter your skin thickness: "))
+no_of_times_admiited = int("Enter the number of times admitted: ")
+hr2_serum_insulin = int(input("Enter number of mLs taken: "))
+
+#main working if tree
