3d41393c824c7f8b57d33a0c2338a5ea903e87a2
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-28 11:14:14 +0530

    initial question commit

---
+++ MIscellaneous/misc7.py
@@ -0,0 +1,9 @@
+"""
+ABC School has allotted unique token IDs from (1 to 
+600) to all the parents for facilitating a lucky draw 
+on the day of their Annual day function. The winner 
+would receive a special prize. Write a program using 
+Python that helps to automate the task.
+"""
+
+print("Annual day Lucky Draw")
