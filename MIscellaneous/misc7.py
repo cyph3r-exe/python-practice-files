1069fdae8bf8d7dc0232b1f1abf506399aea3f80
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-11-28 12:34:08 +0530

    completed answer

--- MIscellaneous/misc7.py
+++ MIscellaneous/misc7.py
@@ -5,5 +5,24 @@ on the day of their Annual day function. The winner
 would receive a special prize. Write a program using 
 Python that helps to automate the task.
 """
+from random import *
 
-print("Annual day Lucky Draw")
+while end_choice == 'y' or end_choice == 'Y':
+
+    print("Annual day Lucky Draw")
+
+    main_choice = input("Would you like to draw? Enter Y for Yes and N for No")
+
+    def test():
+        x = randrange(1,601)
+        return x
+
+    if main_choice == 'y' or main_choice == 'Y':
+        lucky = test()
+        print(f"The lcky number winning the prize is {lucky}")
+    else:
+        end_choice = input("Would you like to continue? Enter Y for Yes and N for No")
+        if end_choice == 'y' or end_choice == 'Y':
+            continue
+        else:
+            break
