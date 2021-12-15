893adfef4ad0b90bcb8165ca6b9664533a2269f6
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2021-12-15 21:00:53 +0530

    usual commit

---
+++ Tkinter/tkmlrn11.py
@@ -0,0 +1,27 @@
+#! string concatenation in python 
+
+#! importing the module
+from tkinter import *
+
+#initialising the root window
+root = Tk()
+
+#Name label 
+nameLabel = Label(root, text="Enter your name here: ")
+nameLabel.grid(row=0, column=0)
+
+#Namee label entry field 
+nameEntry = Entry(root, text = "Name")
+nameEntry.grid(row = 0, column =1)
+
+#defining the function call 
+def myCall():
+    myLabel1 = Label(root, text=f"My name is {nameEntry.get()}")
+    myLabel1.grid(row=2, column=0)
+
+#defining the button
+initButton = Button(root, text="Press me", command = myCall)
+initButton.grid(row=1, column=0)
+
+#running the root window loop 
+root.mainloop()
