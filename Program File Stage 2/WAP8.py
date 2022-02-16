c6e2c99ac2bd782f385226c49c1bcf763aefc43b
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-02-16 15:36:19 +0530

    added new comments

--- Program File Stage 2/WAP8.py
+++ Program File Stage 2/WAP8.py
@@ -1,8 +1,10 @@
 #WAP to accept 10 numbers in a tuple and then display the largest and the smallest number.
 
+#initialising variables 
 x = 1
 emptyTuple = ()
 
+#loops to show the smallest and largest numbers
 while x <=10:
     ranInt = int(input("Enter any Number: "))
     emptyTuple = emptyTuple + (ranInt, )
