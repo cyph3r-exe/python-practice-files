f7d16d4cc9be43c44a6fa27d0d3ac786aa27c1da
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-04-20 23:24:09 +0530

    shifu

---
+++ FileHandling/flhdling3.py
@@ -0,0 +1,29 @@
+from asyncore import read
+
+
+def writingInputOnce():
+    F1 = open("Hello.text", 'w')
+    a = input("Enter a string: ")
+    F1.write(a)
+    F1.close()
+
+def readingInput():
+    F = open("Hello.txt", 'r')
+    A = F.readlines()
+    try:
+        while (F):
+            A = F.readlines()
+            for i in A:
+                B = i.split()
+                for y in B:
+                    if y == 'The':
+                        print(i)
+                    else:
+                        continue            
+    except:
+        print("End of File")
+    F.close()
+        
+
+writingInputOnce()
+readingInput()
