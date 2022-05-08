3226ffffbaa999487061326faadfe189deba32ee
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-05-08 17:44:33 +0530

    add: complete WAP

--- Program File EP2/WAP 7.py
+++ Program File EP2/WAP 7.py
@@ -10,8 +10,87 @@
 #Date of testing str 2
 #rtpcr result str 3
 
-import os 
+from hashlib import new
+import os
+import csv
+from tabnanny import check 
 
+#function to add data to CSV file. 
+
+def addData(data):
+    F = open("Database.csv", 'r')
+    pointer = csv.writer(F)
+    pointer.writerow(data)
+    F.close()
+
+def deleteData(iD):
+    F = open("Database.csv", 'r')
+    F1 = open("temp.csv", 'a')
+    reader = csv.reader(F)
+    writer = csv.writer(F1)
+    for i in reader:
+        if i[0] == iD:
+            continue
+        else:
+            writer.writerow(i)
+    os.remove(F)
+    os.rename("Database.csv", "temp.csv")
+
+def modPatientName(iD, newName):
+    F = open("Database.csv", 'r')
+    F1 = open("temp.csv", 'w')
+    reader = csv.reader(F)
+    writer = csv.writer(F1)
+    for i in reader:
+        if i[0] == iD:
+            i[1] == newName
+            writer.writerow(i)
+        else:
+            writer.writerow(i)
+    os.remove(F)
+    os.rename("temp.csv", "Database.csv")
+
+def modDateofTesting(iD, newDOT):
+    F = open("Database.csv", 'r')
+    F1 = open("temp.csv", 'w')
+    reader = csv.reader(F)
+    writer = csv.writer(F1)
+    for i in reader:
+        if i[0] == iD:
+            i[1] == newDOT
+            writer.writerow(i)
+        else:
+            writer.writerow(i)
+    os.remove(F)
+    os.rename("temp.csv", "Database.csv")
+
+def modResultPOStoNEG(iD):
+    F = open("Database.csv", 'r')
+    F1 = open("temp.csv", 'w')
+    reader = csv.reader(F)
+    writer = csv.writer(F1)
+    for i in reader:
+        if i[0] == iD:
+            i[3] == "-"
+            writer.writerow(i)
+        else:
+            writer.writerow(i)
+    os.remove(F)
+    os.rename("temp.csv", "Database.csv")
+    
+def modResultNEGtoPOS(iD):
+    F = open("Database.csv", 'r')
+    F1 = open("temp.csv", 'w')
+    reader = csv.reader(F)
+    writer = csv.writer(F1)
+    for i in reader:
+        if i[0] == iD:
+            i[3] == "+"
+            writer.writerow(i)
+        else:
+            writer.writerow(i)
+    os.remove(F)
+    os.rename("temp.csv", "Database.csv")
 
 x = "Y"
 Pid = 0
@@ -35,13 +114,13 @@ while (x):
         temp.append(Pdot)
         rtpcr = str(input("Enter Result [+/-]: "))
         temp.append(rtpcr)
-        #! Function Call for writing in CSv file 
+        addData(temp)
 
         x = str(input("Do you wish to continue ? Y / N: "))
 
     elif choice1 == 2:
         checkId = int(input("Enter ID of patient: "))
-        #! Function Call 
+        deleteData(checkId) 
 
     elif choice1 == 3:
         print("What do you want to Modify? ")
@@ -50,23 +129,27 @@ while (x):
         print("3) Patient RTPCR Result")
         choice2 = int(input("Enter your choice: "))
         if choice2 == 1:
+            checkId = int(input("Enter ID of patient: "))
             new_name = str(input("Enter new name: "))
-            #! Function Call
+            modPatientName(checkId, new_name)
+
+            x = str(input("Do you wish to continue ? Y / N: "))
         
         elif choice2 == 2:
+            checkId = int(input("Enter ID of patient: "))
             new_date = str(input("Enter new Date: "))
-            #! Function Call
+            modDateofTesting(checkId, new_date)
+            x = str(input("Do you wish to continue ? Y / N: "))
 
         elif choice2 == 3:
             print("Enter 1 if it's +ve to -ve ")
             print("Enter 2 if it's -ve to +ve ")
             choice3 = int(input("Enter your choice: "))
-            #! Function Call
+            checkId = int(input("Enter ID of patient: "))
+            if choice3 == 1:
+                modResultPOStoNEG(checkId)
+            else:
+                modResultNEGtoPOS(checkId)
             
-
     elif choice1 == 6:
         os._exit()
-
-
-        
-
