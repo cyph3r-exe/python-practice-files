f15263377ab721a284c8e6c1ede03610ed80e627
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-05-10 17:57:02 +0530

    initcom: Adding a file

---
+++ Program File EP2/WAP 9.py
@@ -0,0 +1,24 @@
+#To read a file A.txt and copy only three lettered words to another file, B.txt. Display the content and 
+#size of B.txt
+
+def mainframe():
+    F = open("A.txt", 'r')
+    F1 = open("B.txt", 'a')
+    A = F.readlines()
+    for i in A:
+        x = i.split()
+        for y in x:
+            if len(y) == 3:
+                print(y)
+                F1.write(i, F1)
+            else:
+                continue
+    F1.close()
+    F.close()
+    B = open("B.txt", 'r')
+    d = B.readlines()
+    s = B.tell()
+    print(d)
+    print(s)
+    
+
