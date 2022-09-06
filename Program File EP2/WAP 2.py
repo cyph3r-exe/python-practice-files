10754bc814e9e1b388be61aabbb07abf51206401
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-09-06 08:39:25 +0530

    Test outputs and big fixes

--- Program File EP2/WAP 2.py
+++ Program File EP2/WAP 2.py
@@ -1,26 +1,31 @@
 #Debangshu  Roy 
 # XII - B 
 # 40
+
 #Write a program to count the number of vowels in a text file named 
 #Myfile.txt. 
+
 def enterData(Data):
     F = open("MyFile.txt", 'a+')
     F.writelines(Data)
     F.close()
+
+count = 0
 def readData():
     F = open("MyFile.txt", 'r+')
-    count = 0
-    try:
-        while (F):
-            x = F.read()
-            for i in x:
-                if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
-                    count += 1
-                    print(f"This file has {count} vowels.")
-                else:
-                    continue
-    except:
-        print("End of File")
+    global count
+    x = F.read()
+    for i in x:
+        for j in ['a', 'e', 'i', 'o', 'u']:
+            if i == j:
+                count += 1
+            else:
+                continue
+        else:
+            continue
+
+    print(count)
+        
 choice = "Y"
 while (choice):
     x = input("Enter the string of your choice: ")
@@ -29,5 +34,16 @@ while (choice):
     if choice == "Y" or choice == "y":
         continue
     else:
-        readData()
+        readData()  
         break
+"""
+TEST OUTPUT 1
+Enter the string of your choice: hola mi tale tale ooo om nom nom bhrim krim prem challl
+Do you want to continue? Y / NN
+17
+
+TEST OUTPUT 2 (SAME FILE)
+Enter the string of your choice: idk tum batao araam aata hai dinar se tere mit jaate hai saare ghum 
+Do you want to continue? Y / NN
+45
+"""
