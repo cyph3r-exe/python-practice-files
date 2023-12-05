80ead7ede813a086691b92f3593f34ef033d59fd
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2023-12-05 23:14:16 +0530

    hcrnk prlm sol

--- HackerRank/hckr8.py
+++ HackerRank/hckr8.py
@@ -1,8 +1,7 @@
 #list comprehension
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    mainlist1 = []
-    for i in 
+    x = int(input())    #first dimension
+    y = int(input())    #second dimension
+    z = int(input())    #third dimension
+    n = int(input())    #max sum
+    print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if ((i+j+k)!=n)])
