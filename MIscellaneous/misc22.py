9e4ccb5fbd469e033faecfd235315a53a8ab2b46
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2024-08-29 00:37:41 +0530

    wealllovefusebie

---
+++ MIscellaneous/misc22.py
@@ -0,0 +1,22 @@
+# a = 0
+# while a<10:
+#     print("WE ALL LOVE FUSEBIEEEEEEEE AND THEEEREXXXXXX")
+#     a+=1
+
+# I want to complicate this a bit even more.
+    
+
+#complicating codes unnecessarily is not a good practice BUT I WILL DO IT
+
+a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+b = "WE ALL LOVE FUSEBIEEEEEEEE AND THEEEREXXXXXX"
+
+
+
+for i in b:
+    for j in a:
+        if j == i:
+            print(i)
+            break
+
+        
